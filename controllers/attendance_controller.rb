a64ac6a1068e1b86e6660092c94560ddec394cb7
Tech-a50 <tech-a50@TECH-A50s-MacBook-Air.local>
2019-05-17 12:14:19 +0100

    final product

--- controllers/attendance_controller.rb
+++ controllers/attendance_controller.rb
@@ -34,63 +34,62 @@ class AttendanceController < Sinatra::Base
     foundFlag = false
     entryNumber = 0
     attendance_entries = Attendance.all
-    attendance_entries.each do |entry|
 
+    attendance_entries.each do |entry|
       if entry.student_id == params[:student_id] && entry.attendance_date == params[:attendance_date]
         foundFlag = true
+        entryNumber = entry.attendance_id
+      end
+    end
+
+    if foundFlag == false
+
+      new_attendance = Attendance.new()
+
+      new_attendance.attendance_date = params[:attendance_date]
+
+      status = params[:status]
+      case status
+      when 'On Time'
+        new_attendance.attendance_status_id = 1
+      when 'Slightly late(5 minutes)'
+        new_attendance.attendance_status_id = 2
+      when 'Very late(over 30 minutes)'
+        new_attendance.attendance_status_id = 3
+      when 'Absent'
+        new_attendance.attendance_status_id = 4
+      when 'Authorized Absence'
+        new_attendance.attendance_status_id = 5
       end
 
-      if foundFlag == false
-        puts "Not Found"
-        course_id = params[:id].to_i
-        new_attendance = Attendance.new()
+      new_attendance.student_id = params[:student_id]
+      new_attendance.description = params[:description]
 
-        new_attendance.attendance_date = params[:attendance_date]
+      new_attendance.save
 
-        status = params[:status]
-        case status
-        when 'On Time'
-          new_attendance.attendance_status_id = 1
-        when 'Slightly late(5 minutes)'
-          new_attendance.attendance_status_id = 2
-        when 'Very late(over 30 minutes)'
-          new_attendance.attendance_status_id = 3
-        when 'Absent'
-          new_attendance.attendance_status_id = 4
-        when 'Authorized Absence'
-          new_attendance.attendance_status_id = 5
-        end
+    elsif foundFlag == true
+      editAttendance = Attendance.find entryNumber
 
-        new_attendance.student_id = params[:student_id]
-        new_attendance.description = params[:description]
+      editAttendance.attendance_date = params[:attendance_date]
 
-        new_attendance.save()
-
-
-      elsif foundFlag == true
-        editAttendance = Attendance.find entryNumber
-
-        editAttendance.attendance_date = params[:attendance_date]
-
-        status = params[:status]
-        case status
-        when 'On Time'
-          editAttendance.attendance_status_id = 1
-        when 'Slightly late(5 minutes)'
-          editAttendance.attendance_status_id = 2
-        when 'Very late(over 30 minutes)'
-          editAttendance.attendance_status_id = 3
-        when 'Absent'
-          editAttendance.attendance_status_id = 4
-        when 'Authorized Absence'
-          editAttendance.attendance_status_id = 5
-        end
-
-        editAttendance.student_id = params[:student_id]
-        editAttendance.description = params[:description]
-
-        editAttendance.save
+      status = params[:status]
+      case status
+      when 'On Time'
+        editAttendance.attendance_status_id = 1
+      when 'Slightly late(5 minutes)'
+        editAttendance.attendance_status_id = 2
+      when 'Very late(over 30 minutes)'
+        editAttendance.attendance_status_id = 3
+      when 'Absent'
+        editAttendance.attendance_status_id = 4
+      when 'Authorized Absence'
+        editAttendance.attendance_status_id = 5
       end
+
+      editAttendance.student_id = params[:student_id]
+      editAttendance.description = params[:description]
+
+      editAttendance.save
     end
       course_id = params[:id].to_i
 
